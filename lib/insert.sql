9d108babbc0086afda61e5fa3e6f1feaca3e14be
josephmangara <josephmangara2@gmail.com>
2023-12-16 16:31:16 +0300

    sql queries

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 11, 'M', 'brown', 'angry', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 9, 'M', 'black', 'happy', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'white', 'quite', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 13, 'M', 'brown', 'irritable', 'False');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 8, 'F', 'black', 'happy', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 24, 'M', 'white', 'spiteful', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 13, 'F', 'brown', 'calm', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, 'F', 'white', 'quite', 'True');
+
